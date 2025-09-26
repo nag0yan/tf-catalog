cac9241152decf50098427463ec5effc7a4af534
nag0yan <matsui-ka@kadokawa.jp>
2025-09-26 17:30:13 +0900

    base

---
+++ modules/webcore/variables.tf
@@ -0,0 +1,20 @@
+variable "service" {
+  description = "The name of the service."
+  type        = string
+}
+
+variable "account_id" {
+  description = "The AWS account ID.(Optional)"
+  type        = string
+  default     = null
+}
+
+variable "region" {
+  description = "The AWS region.(Optional)"
+  type        = string
+  default     = null
+  validation {
+    condition = var.region == null || contains(local.allowed_region, var.region)
+    error_message = "The specified region is not supported. Supported regions are: ${join(", ", local.allowed_region)}"
+  }
+}
